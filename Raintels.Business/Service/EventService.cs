9c06e5d9ea09e77e68afc8dcdab91311f992c832
serosh k vikraman <serosh.vikraman@outlook.com>
2021-06-26 16:58:25 +0530

    Time format change

--- Raintels.Business/Service/EventService.cs
+++ Raintels.Business/Service/EventService.cs
@@ -6,6 +6,7 @@ using Raintels.Entity.ViewModel;
 using Raintels.Service.ServiceInterface;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -29,16 +30,16 @@ namespace Raintels.Service
 
         public async Task<EventViewModel> CreateEvent(EventViewModel eventViewModel)
         {
-           
+
             var eventDataModel = mapper.Map<EventDataModel>(eventViewModel);
             eventDataModel = await eventManager.CreateEvent(eventDataModel);
             var eventViewModelReturn = mapper.Map<EventViewModel>(eventViewModel);
             return eventViewModelReturn;
         }
 
-        public async Task<List<EventViewModel>> GetEvent(long userId, long EventId,string EventCode)
+        public async Task<List<EventViewModel>> GetEvent(long userId, long EventId, string EventCode)
         {
-            var events = await eventManager.GetEvent(userId,EventId, EventCode);
+            var events = await eventManager.GetEvent(userId, EventId, EventCode);
             List<EventViewModel> studentsViewModel = new List<EventViewModel>();
             foreach (var item in events)
             {
@@ -47,16 +48,16 @@ namespace Raintels.Service
                     {
                         EventID = item.EventID,
                         EventName = item.EventName,
-                        EventDetails=item.EventDetails,
+                        EventDetails = item.EventDetails,
                         EventCode = item.EventCode,
-                        EventStartDateTIme = item.EventStartDateTIme,
-                        EventEndDateTIme = item.EventEndDateTIme,
+                        EventStartDateTIme = Convert.ToDateTime(item.EventStartDateTIme).ToString("yyyy-MM-dd HH:mm:ss"),
+                        EventEndDateTIme = Convert.ToDateTime(item.EventEndDateTIme).ToString("yyyy-MM-dd HH:mm:ss")
                     });
             }
             return studentsViewModel;
         }
 
-       
+
 
         public async Task<EventAnalyticsViewModel> ManageEventAnalysis(EventAnalyticsViewModel eventAnalyticsViewModel, int type)
         {
@@ -78,7 +79,7 @@ namespace Raintels.Service
                         EventID = item.EventID,
                         QnACount = item.QnACount,
                         QnALikeCount = item.QnALikeCount,
-                        
+
                     });
             }
             return analytiData;
@@ -97,7 +98,7 @@ namespace Raintels.Service
                 select new XElement("TableDetail",
                              new XElement("PollID", ObjDetails.PollID),
                              new XElement("OptionTitle", ObjDetails.OptionTitle),
-                             new XElement("isCorrect",  ObjDetails.isCorrect==true?1:0),
+                             new XElement("isCorrect", ObjDetails.isCorrect == true ? 1 : 0),
                              new XElement("IsActive", ObjDetails.IsActive == true ? 1 : 0)
                            ));
 
@@ -105,8 +106,8 @@ namespace Raintels.Service
 
             pollDataModel = await eventManager.savePoll(pollDataModel);
 
-           var PollViewModelReturn = mapper.Map<PollViewModel>(pollDataModel);
-          return PollViewModelReturn;
+            var PollViewModelReturn = mapper.Map<PollViewModel>(pollDataModel);
+            return PollViewModelReturn;
         }
 
         public async Task<List<PollUserViewModel>> GetPollByCode(string EventCode)
@@ -131,7 +132,7 @@ namespace Raintels.Service
 
         public async Task<List<PollOptionsViewModel>> GetPollOptions(long PollId)
         {
-            var epollList  = await eventManager.GetPollOptions(PollId);
+            var epollList = await eventManager.GetPollOptions(PollId);
             List<PollOptionsViewModel> analytiData = new List<PollOptionsViewModel>();
             foreach (var item in epollList)
             {
